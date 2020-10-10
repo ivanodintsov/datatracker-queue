ea0039788956feb0ebdeb39a9133880181c6c514
Ivan Odintsov <iwan.odintsov@gmail.com>
2020-10-11 00:25:00 +0300

    empty config added

---
+++ datatracker-queue-app/config.mjs
@@ -0,0 +1,13 @@
+export const TG_BOT_TOKEN = '';
+export const API_URL = '';
+export const API_TOKEN = '';
+
+export const imgurConfig = {
+  clientId: '',
+  albums: {
+    chats: {
+      id: '',
+      deletehash: '',
+    },
+  },
+};
